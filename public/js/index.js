70581b26178fa73f1f4cd2d6dcc63aa618d71ceb
twnbat <obeliskz8official@gmail.com>
2025-10-04 23:10:56 +1300

    working version

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,26 @@
 import * as ChallengeHandler from './api/ChallengeHandler.js'
+import * as Utils from './util/Utils.js'
 /** @type {HTMLCollectionOf<Element>} */
 const numButtons = document.getElementsByClassName("numbutton")
 const challengeNumberDisplay = document.getElementById("numdisplay")
 const contentDisplay = document.getElementById("content")
 const searchButton = document.getElementById("go")
 const { firstAndLastChallengeNumbers } = await ChallengeHandler.getFirstAndLastChallenges()
+const difficultyFace = document.getElementById("difficultyface")
+const idButton = document.getElementById("idbutton")
+const copynotif = document.getElementById("copynotif")
+const domObjectIdToLevelProperty = {
+  stars: "stars",
+  requestedstars: "requestedstars",
+  challnumber: "challengeNumber",
+  song: "song",
+  levelid: "id",
+  difficulty: "difficulty",
+  description: "description",
+  levelname: "name"
+};
+
+
 let challengeNumber = 0
 
 function setChallengeNumber(num) {
@@ -30,11 +46,38 @@ for (let button of numButtons) {
     })
 }
 
-searchButton.addEventListener("click", async () => {
-    const chall = await ChallengeHandler.getChallengeFromChallengeNumber(challengeNumber)
-    console.log(chall.challenge.essential)
-    contentDisplay.textContent = JSON.stringify(chall, null, 2)
+idButton.addEventListener("click", async () => {
+    const idText = document.getElementById("levelid")
+    if (!isNaN(idText.textContent)) {
+        try {
+            await navigator.clipboard.writeText(idText.textContent)
+            copynotif.classList.remove("hidden")
+            setTimeout(() => {
+                copynotif.classList.add("hidden")
+            }, 2000)
+        } catch (error) {
+            console.log("Copy failed", error)
+        }
+    }
 })
+
+searchButton.addEventListener("click", async () => {
+    const level = await ChallengeHandler.getChallengeFromChallengeNumber(challengeNumber)
+    console.log(level)
+    for (const [id, field] of Object.entries(domObjectIdToLevelProperty)) {
+        
+        const element = document.getElementById(id)
+        if (id === "stars") {
+            if (level.challenge.essential[field] === 0) {
+                element.textContent = "Unrated"
+                continue
+            }
+        }
+        element.textContent = level.challenge.essential[field]
+    }
+    difficultyFace.src = "../assets/difficultyfaces/" + Utils.difficultyToFace[level.challenge.essential.difficulty] + ".png"
+})
+
 
 
 
---
+++ public/js/util/Utils.js
@@ -0,0 +1,14 @@
+export const difficultyToFace = {
+    "N/A": "unrated",
+    "Auto": "auto",
+    "Easy": "easy",
+    "Normal": "normal",
+    "Hard": "hard",
+    "Harder": "harder",
+    "Insane": "insane",
+    "Easy Demon": "demon-easy",
+    "Medium Demon": "demon-medium",
+    "Hard Demon": "demon-hard",
+    "Insane Demon": "demon-insane",
+    "Extreme Demon": "demon-extreme"
+}
