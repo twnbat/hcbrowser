84e08472c80db70c600b1e3ea3c77bd5bcc24d29
twnbat <obeliskz8official@gmail.com>
2025-10-06 17:43:39 +1300

    Port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const liveReloadServer = livereload.createServer();
 liveReloadServer.watch(join(__dirname, 'public'));
